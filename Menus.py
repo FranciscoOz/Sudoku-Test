8d995e46e48e894c8871cea7be2bbf233ff9cb9b
FranciscoOz <73808912+FranciscoOz@users.noreply.github.com>
2020-11-02 21:05:33 -0500

    Menu_0.1
    Se añadió función de guardado y carga

--- Menus.py
+++ Menus.py
@@ -1,1 +1,23 @@
-print("Hola mundo")
+matriz_nombres_guardados =["PEPE"]
+matriz_tableros_guardados=[["pepito"]]
+matriz = [["j"],["an"]]
+#Guardar partida
+def guardaPartida(matriz):
+    nombre_jugador=input("Ingrese nombre de la partida/jugador").upper()
+    matriz_tableros_guardados.append([matriz])
+    matriz_nombres_guardados.append(nombre_jugador)
+
+guardaPartida(matriz)
+
+print(matriz_nombres_guardados)
+print(matriz_tableros_guardados)
+
+#Carga partida
+def cargaPartida():
+    nombre_jugador = input("Ingresa nombre de tablero/jugador: ").upper()
+    posicion = int(matriz_nombres_guardados.index(nombre_jugador))
+    matriz_resultado = matriz_tableros_guardados[posicion]
+    return matriz_resultado
+cargaPartida()
+print(matriz_tableros_guardados)
+print(matriz_nombres_guardados)
