bbb8718352866576a77ebc55d123dbbd0109db5c
FranciscoOz <73808912+FranciscoOz@users.noreply.github.com>
2020-11-04 13:06:03 -0500

    Sudoku-0.1_2 (Sin reglas)
    Se agrega las funciones de Crear tablero nuevo y Cargar partida (Sin archivos adicionales)

--- Reglas.py
+++ Reglas.py
@@ -1,2 +1,11 @@
-print("Hola mundo")
-fnsjncnck
+#Mismos números en fila o columna
+def Regla_1(matriz)
+    Cumple = False
+    for i in range (9):
+        for j in range(9):
+            matriz[i][j]
+    return Cumple
+
+
+
+#Si ingresa letra vuelve a pedir dato o da error
--- main.py
+++ main.py
@@ -1,32 +1,7 @@
 import random as random
-#Hola carebola
-
-#CREA TABLERO DE JUEGO 9X9 CON 0's
-tablero_principal = []
-for i in range (9):
-    tablero_principal.append([])
-    for j in range (9):
-        tablero_principal[i].append(0)
-
-#RELLENAMOS CON NUMEROS ALEATORIOS ALGUNAS CASILLAS SEGÚN CONTADOR Y CUMPLIENDO LAS REGLAS
-contador = 19
-Cumple = True
-for i in range (9):
-  for j in range (9):
-    if tablero_principal[i][j]==tablero_principal[i+1][j+1] Cumpe
-    if contador!=0 and Cumple:
-      tablero_principal[random.randrange(1,9)][random.randrange(1,9)]=random.randrange(1,9)
-      contador-=1
 
 
-#CREA TABLERO VACIO
-for i in range(9):
-        if i%3==0:
-            print("- - - - - - - - - - - - - ")
-        for j in range(9):
-            if j%3==0:
-                print(" | ", end="")
-            if j== 8:
-                print(tablero_principal[i][j])
-            else:
-                print(str(tablero_principal[i][j]) + " ", end="")
+tablero_principal = CreaMatrizInicial()
+ 
+
+CreaTablero(tablero_principal)
