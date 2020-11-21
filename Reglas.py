01f6be0da1e4181d54cd0405c19b3d7573341645
FranciscoOz <73808912+FranciscoOz@users.noreply.github.com>
2020-11-21 02:29:17 -0500

    Update Reglas.py

--- Reglas.py
+++ Reglas.py
@@ -1,11 +1,18 @@
 #Mismos números en fila o columna
-def Regla_1(matriz)
+def Regla_1(matriz):
     Cumple = False
     for i in range (9):
         for j in range(9):
             matriz[i][j]
     return Cumple
-
+def borraRepetidos(lista):
+    nueva=[]
+    for elemento in lista:
+        if not elemento in nueva:
+            nueva.append(elemento)
+        else:
+            nueva.append(0)
+    return nueva
 
 
 #Si ingresa letra vuelve a pedir dato o da error
