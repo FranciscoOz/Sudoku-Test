8bff77060f221bc4c351a1bc13c29b9e98c66a68
FranciscoOz <francisco.osores@utec.edu.pe>
2021-04-18 07:47:56 -0500

    Sudoku_2.1.0
    -Project functions were optimized
    -Redundancy in the code was eliminated
    -Now the functions have particular names

--- Menu.py
+++ Menu.py
@@ -2,31 +2,45 @@ import json
 from Reglas import *
 import random
 
+
+class Clr:
+    PP = '\033[95m'
+    CN = '\033[96m'
+    DC = '\033[36m'
+    BL = '\033[94m'
+    GR = '\033[92m'
+    GR2 = '\033[34m'
+    YW = '\033[93m'
+    RD = '\033[91m'
+    BD = '\033[1m'
+    UL = '\033[4m'
+    END = '\033[0m'
+    WH = '\033[97m'
+
+    def colorAlAzar(self):
+        index = random.randint(0, 10)
+        listaColors = ['\033[31m', '\033[32m', '\033[33m', '\033[34m', '\033[35m', '\033[36m', '\033[91m', '\033[92m',
+                       '\033[93m', '\033[94m', '\033[95m', '\033[96m', ]
+        return print(listaColors[index], end="")
+
+
 matriz_nombres_guardados = list()
 matriz_tableros_guardados = list()
 
-
-def VerificaExistencia(archivo):
-    try:
-        with open(archivo, 'r') as f:
-            return True
-    except FileNotFoundError as e:
-        return False
-    except IOError as e:
-        return False
-
+# JSON
 if not VerificaExistencia("sudoku.json"):
-    DBsudoku= open("sudoku.json","w+")
-    dicNuevo =dict()
-    json.dump(dicNuevo,DBsudoku)
+    DBsudoku = open("sudoku.json", "w+")
+    dicNuevo = dict()
+    json.dump(dicNuevo, DBsudoku)
     DBsudoku.close()
+
 
 def leeSudokus():
     global matriz_tableros_guardados, matriz_nombres_guardados
     matriz_nombres_guardados = []
     matriz_tableros_guardados = []
-    archi=open("sudoku.json", "r")
-    jSud=json.load(archi)
+    archi = open("sudoku.json", "r")
+    jSud = json.load(archi)
     archi.close()
     jSudNom = jSud.keys()
     jSudTab = jSud.values()
@@ -35,54 +49,57 @@ def leeSudokus():
     for i in jSudTab:
         matriz_tableros_guardados.append(i)
 
-class Clr:
-   PP = '\033[95m'
-   CN = '\033[96m'
-   DC = '\033[36m'
-   BL = '\033[94m'
-   GR = '\033[92m'
-   GR2 = '\033[34m'
-   YW = '\033[93m'
-   RD = '\033[91m'
-   BD = '\033[1m'
-   UL = '\033[4m'
-   END = '\033[0m'
-   WH = '\033[30m'
-   def colorAlAzar(self):
-       index = random.randint(0, 10)
-       listaColors = ['\033[31m', '\033[32m', '\033[33m','\033[34m', '\033[35m', '\033[36m','\033[91m', '\033[92m', '\033[93m','\033[94m', '\033[95m', '\033[96m',]
-       return print(listaColors[index], end="")
 
+def IntroduceJSON():
+    DICtemp = dict()
+    archi = open("sudoku.json", "w")
+    for i in range(len(matriz_tableros_guardados)):
+        DICtemp[matriz_nombres_guardados[i]] = matriz_tableros_guardados[i]
+        if matriz_tableros_guardados[i] == "vacío":
+            del DICtemp[matriz_nombres_guardados[i]]
+    json.dump(DICtemp, archi)
+    archi.close()
+
+
+# Menus
 def MenuPrincipal():
     print()
-    print(Clr.WH+"🎅♥☻♠☻♣☻♦☻ - - SUDOKU 2.0 - - ☻♦☻♣☻♠☻♥🎅"+Clr.END)
-    print(Clr.CN+"🎅                                          🎅"+Clr.END)
-    print(Clr.CN+"🎅    "+Clr.END, Clr.YW+"🏆🏆🏆"+Clr.END,Clr.BL+Clr.BD+" MENÚ PRINCIPAL"+Clr.END+Clr.END, Clr.YW+" 🏆🏆          "+Clr.END, Clr.CN+f"🎅"+Clr.END)
-    print(Clr.CN+"🎅                                          🎅"+Clr.END)
-    print(Clr.CN+"🎅    "+Clr.END,Clr.WH+"❄❄"+Clr.END, Clr.GR2+Clr.BD+"(1) Jugar tablero nuevo"+Clr.END+Clr.END, "        ", Clr.CN+"🎅"+Clr.END)
-    print(Clr.CN+"🎅    "+Clr.END,Clr.WH+"❄❄"+Clr.END, Clr.GR2+Clr.BD+"(2) Ver/Jugar tablero guardado"+Clr.END+Clr.END, " ", Clr.CN+"🎅"+Clr.END)
-    print(Clr.CN+"🎅    "+Clr.END,Clr.WH+"❄❄"+Clr.END, Clr.GR2+Clr.BD+"(3) Finalizar"+Clr.END+Clr.END, "                  ", Clr.CN+"🎅"+Clr.END)
-    print(Clr.CN+"🎅                                          🎅"+Clr.END)
-    print(Clr.DC+"🎅♥☻♠☻♣☻♦☻ ------------------ ☻♦☻♣☻♠☻♥🎅"+Clr.END)
+    print(Clr.WH + "🎅♥☻♠☻♣☻♦☻ - - SUDOKU 2.0 - - ☻♦☻♣☻♠☻♥🎅" + Clr.END)
+    print(Clr.CN + "🎅                                          🎅" + Clr.END)
+    print(Clr.CN + "🎅    " + Clr.END, Clr.YW + "🏆🏆🏆" + Clr.END,
+          Clr.BL + Clr.BD + " MENÚ PRINCIPAL" + Clr.END + Clr.END, Clr.YW + " 🏆🏆          " + Clr.END,
+          Clr.CN + f"🎅" + Clr.END)
+    print(Clr.CN + "🎅                                          🎅" + Clr.END)
+    print(Clr.CN + "🎅    " + Clr.END, Clr.WH + "❄❄" + Clr.END,
+          Clr.GR2 + Clr.BD + "(1) Jugar tablero nuevo" + Clr.END + Clr.END, "        ", Clr.CN + "🎅" + Clr.END)
+    print(Clr.CN + "🎅    " + Clr.END, Clr.WH + "❄❄" + Clr.END,
+          Clr.GR2 + Clr.BD + "(2) Ver/Jugar tablero guardado" + Clr.END + Clr.END, " ", Clr.CN + "🎅" + Clr.END)
+    print(Clr.CN + "🎅    " + Clr.END, Clr.WH + "❄❄" + Clr.END, Clr.GR2 + Clr.BD + "(3) Finalizar" + Clr.END + Clr.END,
+          "                  ", Clr.CN + "🎅" + Clr.END)
+    print(Clr.CN + "🎅                                          🎅" + Clr.END)
+    print(Clr.DC + "🎅♥☻♠☻♣☻♦☻ ------------------ ☻♦☻♣☻♠☻♥🎅" + Clr.END)
     opcion = input()
     return menuOpciones(opcion)
 
+
 def menuOpciones(opcion):
     if opcion == "1":
-        print(Clr.DC+" --------------------"+Clr.END)
-        print(Clr.DC+"|"+Clr.END," 👇" ,Clr.BD+Clr.WH+" Dificultad"+Clr.END+Clr.END,end="")
-        print(Clr.DC+"    |"+Clr.END)
-        print(Clr.DC+"|"+Clr.END, Clr.BD+"(0)", Clr.GR+" Fácil   (🤡) "+Clr.END+Clr.END,Clr.DC+"|"+Clr.END)
-        print(Clr.DC+"|"+Clr.END, Clr.BD+"(1)", Clr.YW+" Medio   (😈) "+Clr.END+Clr.END,Clr.DC+"|"+Clr.END)
-        print(Clr.DC+"|"+Clr.END, Clr.BD+"(2)", Clr.RD+" Difícil (💀) "+Clr.END+Clr.END,Clr.DC+"|"+Clr.END)
-        print(Clr.DC+" --------------------"+Clr.END)
+        print(Clr.DC + " --------------------" + Clr.END)
+        print(Clr.DC + "|" + Clr.END, " 👇", Clr.BD + Clr.WH + " Dificultad" + Clr.END + Clr.END, end="")
+        print(Clr.DC + "    |" + Clr.END)
+        print(Clr.DC + "|" + Clr.END, Clr.BD + "(1)", Clr.GR + " Fácil   (🤡) " + Clr.END + Clr.END,
+              Clr.DC + "|" + Clr.END)
+        print(Clr.DC + "|" + Clr.END, Clr.BD + "(2)", Clr.YW + " Medio   (😈) " + Clr.END + Clr.END,
+              Clr.DC + "|" + Clr.END)
+        print(Clr.DC + "|" + Clr.END, Clr.BD + "(3)", Clr.RD + " Difícil (💀) " + Clr.END + Clr.END,
+              Clr.DC + "|" + Clr.END)
+        print(Clr.DC + " --------------------" + Clr.END)
         dificultad = input()
         sudoku = CreaMatrizInicial()
-        RellenaCuadrante(sudoku, 1)
-        RellenaCuadrante(sudoku, 5)
-        RellenaCuadrante(sudoku, 9)
-        Resolver_cifra(sudoku)
-        if dificultad == "0" or dificultad == "1" or dificultad == "2":
+        RellenaCuadrante(sudoku)
+        ResuelveCifra(sudoku)
+        mode = ["1", "2", "3"]
+        if dificultad in mode:
             RellenaCeros(sudoku, dificultad)
         else:
             print("Escoge una dificultad válida >:)")
@@ -92,39 +109,41 @@ def menuOpciones(opcion):
         print()
         return sudoku
 
-    elif opcion=="2":
+    elif opcion == "2":
         print()
-        print("Cargando tableros...",end="")
+        print("Cargando tableros...", end="")
         Menu1()
 
     elif opcion == "3":
-        print(Clr.WH+Clr.BD+"- - Fin de juego - -")
+        print(Clr.WH + Clr.BD + "- - Fin de juego - -")
         return IntroduceJSON()
     else:
         print("Ingresa una opción válida entre 1 y 3 ☻\n")
         print()
         return MenuPrincipal()
 
+
 def Menu1():
     leeSudokus()
     print()
     print()
-    print(Clr.BD +" ____________________"+ Clr.END)
-    print(Clr.BD +"|"+ Clr.END, "\033[03m" +Clr.BD + "\033[93m"+ "TABLEROS GUARDADOS" + Clr.END , Clr.BD +"|"+ Clr.END)
+    print(Clr.BD + " ____________________" + Clr.END)
+    print(Clr.BD + "|" + Clr.END, "\033[03m" + Clr.BD + "\033[93m" + "TABLEROS GUARDADOS" + Clr.END,
+          Clr.BD + "|" + Clr.END)
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-    c=0
+    c = 0
     for i in matriz_nombres_guardados:
         if matriz_tableros_guardados[c] != "vacío":
             Clr().colorAlAzar()
-            print("\033[03m"+i+Clr.END)
+            print("\033[03m" + i + Clr.END)
             print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
         c += 1
 
-    #Comprobar si no hay matrices
+    # Comprobar si no hay matrices
     EstaVacio = True
     for i in matriz_tableros_guardados:
-        if i!="vacío":
-            EstaVacio=False
+        if i != "vacío":
+            EstaVacio = False
 
     if EstaVacio:
         print("No hay tableros guardados")
@@ -160,11 +179,12 @@ def Menu1():
             print("Elije una opción válida")
             return Menu1()
 
-def Menu2(NameTotal,tab):
+
+def Menu2(NameTotal, tab):
     print()
-    print("|", "👉 (1) Cargar partida",Clr.CN+ "🔻"+Clr.END)
-    print("|", "👉 (2) Borrar Partida","\033[31m"+ "❌"+Clr.END)
-    print("|", "👉 (3) Regresar a las partidas guardadas",Clr.BD +"↩"+Clr.END)
+    print("|", "👉 (1) Cargar partida", Clr.CN + "🔻" + Clr.END)
+    print("|", "👉 (2) Borrar Partida", "\033[31m" + "❌" + Clr.END)
+    print("|", "👉 (3) Regresar a las partidas guardadas", Clr.BD + "↩" + Clr.END)
     opcion = input("")
     if opcion == "1":
         return continuaTablero(tab)
@@ -179,34 +199,23 @@ def Menu2(NameTotal,tab):
         print()
         return Menu2(NameTotal, tab)
 
-def HayCeros(sudoku):
-    HayCeros = 0
-    for i in range(9):
-        for j in range(9):
-            if sudoku[i][j] == "0":
-                HayCeros = True
-    return HayCeros
 
 def continuaTablero(sudoku):
-    NoHaySolucion = False
-    if NoHaySolucion:
-        print("No hay solución :c ")
-        NoHaySolucion = True
     MuestraTablero(sudoku)
     print()
-    print(Clr.YW+"◤--", "                 ", "--◥")
-    print(Clr.YW+"|", Clr.WH+"(0) Seguir jugando"+Clr.END, "   ", Clr.YW+"|")
-    print(Clr.YW+"|", Clr.WH+"(1) Solucionar tablero" + Clr.END, Clr.YW + "|")
-    print(Clr.YW+"|", Clr.WH+"(2) Guardar partida"+Clr.END, "  ", Clr.YW+"|")
-    print(Clr.YW+"|", Clr.WH+"(3) Volver al menu"+Clr.END, "   ", Clr.YW+"|")
-    print(Clr.YW+"◣--", "                 ", "--◢")
+    print(Clr.YW + "◤--", "                 ", "--◥")
+    print(Clr.YW + "|", Clr.WH + "(0) Seguir jugando" + Clr.END, "   ", Clr.YW + "|")
+    print(Clr.YW + "|", Clr.WH + "(1) Solucionar tablero" + Clr.END, Clr.YW + "|")
+    print(Clr.YW + "|", Clr.WH + "(2) Guardar partida" + Clr.END, "  ", Clr.YW + "|")
+    print(Clr.YW + "|", Clr.WH + "(3) Volver al menu" + Clr.END, "   ", Clr.YW + "|")
+    print(Clr.YW + "◣--", "                 ", "--◢")
     opcion = input()
     if opcion == "0":
         return modValor(sudoku)
-    elif opcion=="1":
-        if HayCeros(sudoku):
-            Resolver_cifra(sudoku)
-            if HayCeros(sudoku):
+    elif opcion == "1":
+        if BuscaVacio(sudoku):
+            ResuelveCifra(sudoku)
+            if BuscaVacio(sudoku):
                 print("No hay solución :c")
         else:
             print("Ya está resuelto :)")
@@ -223,11 +232,12 @@ def continuaTablero(sudoku):
         print("Ingresa una opción válida entre 1 y 3 ☻")
         return continuaTablero(sudoku)
 
+
 def guardaPartida(matriz):
     leeSudokus()
     nombre_jugador = input("Ingrese nombre del jugador: ").upper()
     nombre_tablero = input("Ingrese nombre de la partida: ").lower()
-    nombre_total = nombre_jugador+": "+nombre_tablero
+    nombre_total = nombre_jugador + ": " + nombre_tablero
     if nombre_total not in matriz_nombres_guardados:
         matriz_tableros_guardados.append(matriz)
         matriz_nombres_guardados.append(nombre_total)
@@ -237,33 +247,35 @@ def guardaPartida(matriz):
         print("(1) Sí")
         print("(2) No")
         opcion = input()
-        if opcion=="1":
+        if opcion == "1":
             index = int()
             for i in matriz_nombres_guardados:
-                if i==nombre_total:
-                    index=matriz_nombres_guardados.index(i)
-            matriz_tableros_guardados[index]=matriz
+                if i == nombre_total:
+                    index = matriz_nombres_guardados.index(i)
+            matriz_tableros_guardados[index] = matriz
             IntroduceJSON()
-        elif opcion=="2":
+        elif opcion == "2":
             guardaPartida(matriz)
         else:
             print("Ingrese una opción válida")
 
     return continuaTablero(matriz)
 
+
 def cargaPartida():
     nombre_jugador = input("Ingresa nombre de jugador: ").upper()
     nombre_partida = input("ingresa nombre de partida: ").lower()
-    nombreTotal = nombre_jugador+": "+nombre_partida
+    nombreTotal = nombre_jugador + ": " + nombre_partida
     if nombreTotal in matriz_nombres_guardados:
         posicion = int(matriz_nombres_guardados.index(nombreTotal))
         matriz_resultado = matriz_tableros_guardados[posicion]
         print()
         MuestraTablero(matriz_resultado)
-        return Menu2(nombreTotal,matriz_resultado)
+        return Menu2(nombreTotal, matriz_resultado)
     else:
         print(f"No se encontró la partida {nombre_partida} del jugador {nombre_jugador}")
         return Menu1()
+
 
 def borraPartida(nombre):
     IndexM = matriz_nombres_guardados.index(nombre)
@@ -271,103 +283,85 @@ def borraPartida(nombre):
     IntroduceJSON()
     return Menu1()
 
-def IntroduceJSON():
-    DICtemp = dict()
-    archi = open("sudoku.json", "w")
-    for i in range(len(matriz_tableros_guardados)):
-        DICtemp[matriz_nombres_guardados[i]] = matriz_tableros_guardados[i]
-        if matriz_tableros_guardados[i] == "vacío":
-            del DICtemp[matriz_nombres_guardados[i]]
-    json.dump(DICtemp, archi)
-    archi.close()
 
+def MuestraTablero(tablero_principal):
+    print(Clr.WH + Clr.BD + "  | 1  2  3  | 4  5  6  | 7  8  9 |" + Clr.END + Clr.END)
+    for i in range(9):
+        if i % 3 == 0:
+            print(Clr.WH + "➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖" + Clr.END)
+            print(Clr.WH + "------------------------------------" + Clr.END)
+        print(Clr.WH + Clr.BD + f"{i + 1}" + Clr.END + Clr.END, end=" ")
+        for j in range(9):
+            if j % 3 == 0:
+                print(Clr.WH + Clr.BD + "| " + Clr.END + Clr.END, end="")
+            if j == 8:
+                if tablero_principal[i][j] == "0":
+                    print(Clr.WH + Clr.BD + str(tablero_principal[i][j]) + Clr.END + Clr.END,
+                          Clr.WH + Clr.BD + "|" + Clr.END + Clr.END)
+                else:
+                    print(Clr.PP + tablero_principal[i][j] + Clr.END, Clr.WH + Clr.BD + "|" + Clr.END + Clr.END)
+            else:
+                if tablero_principal[i][j] == "0":
+                    print(Clr.WH + Clr.BD + str(tablero_principal[i][j]) + Clr.END + Clr.END + "  ", end="")
+                else:
+                    print(Clr.PP + str(tablero_principal[i][j]) + Clr.END + "  ", end="")
+        if i == 8:
+            print(Clr.WH + "➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖" + Clr.END)
+
+
+# Funcionalidades
 def CreaMatrizInicial():
-    tablero= []
+    tablero = []
     for i in range(9):
         tablero.append([])
         for j in range(9):
             tablero[i].append("0")
     return tablero
 
-def RellenaCuadrante(tabla, cuadrante):
-    disponibles = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    minifilas = filaCuadrante(cuadrante)
-    minicolumnas = columnaCuadrante(cuadrante)
-    for i in minifilas:
-        for j in minicolumnas:
-            longitud = len(disponibles)
-            aleatorio = random.randint(0, longitud - 1)
-            tabla[i][j] = disponibles[aleatorio]
-            disponibles.remove(disponibles[aleatorio])
 
-def MuestraTablero(tablero_principal):
-    print(Clr.WH+Clr.BD+"  | 1  2  3  | 4  5  6  | 7  8  9 |"+Clr.END+Clr.END)
-    for i in range(9):
-        if i % 3 == 0:
-            print(Clr.WH+"➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖"+Clr.END)
-        print(Clr.WH+Clr.BD+f"{i + 1}"+Clr.END+Clr.END, end=" ")
-        for j in range(9):
-            if j % 3 == 0:
-                print(Clr.WH+Clr.BD+"| "+Clr.END+Clr.END, end="")
-            if j == 8:
-                if tablero_principal[i][j] == "0":
-                    print(Clr.WH+Clr.BD+str(tablero_principal[i][j])+Clr.END+Clr.END, Clr.WH+Clr.BD+"|"+Clr.END+Clr.END)
-                else:
-                    print(Clr.PP+tablero_principal[i][j]+Clr.END, Clr.WH+Clr.BD+"|"+Clr.END+Clr.END)
-            else:
-                if tablero_principal[i][j] == "0":
-                    print(Clr.WH+Clr.BD+str(tablero_principal[i][j])+Clr.END+Clr.END + "  ", end="")
-                else:
-                    print(Clr.PP+str(tablero_principal[i][j])+Clr.END + "  ", end="")
-        if i == 8:
-            print(Clr.WH+"➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖"+Clr.END)
+def RellenaCuadrante(tabla):
+    for n in range(3):
+        disponibles = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        for i in range(n * 3, (n * 3) + 3):
+            for j in range(n * 3, (n * 3) + 3):
+                tabla[i][j] = random.choice(disponibles)
+                disponibles.remove(tabla[i][j])
+
 
 def RellenaCeros(tabla, nivel):
-    maxceros=0
-    if nivel == "0":
-        maxceros = 30
-    elif nivel == "1":
-        maxceros = 40
+    maxceros = 0
+    if nivel == "1":
+        maxceros = 0
     elif nivel == "2":
+        maxceros = 40
+    elif nivel == "3":
         maxceros = 60
     for i in range(maxceros):
         n = random.randint(0, 8)
         m = random.randint(0, 8)
         tabla[n][m] = "0"
 
+
 def modValor(matriz):
-    PosiblesNetos = []
-    filita = input("👉 Ingrese fila    : ")
-    columnita = input("👉 Ingrese columna : ")
+    fila = input("👉 Ingrese fila    : ")
+    columna = input("👉 Ingrese columna : ")
     valor = input("👉 Ingrese valor   : ")
     print()
-    if Regla_1(filita) and Regla_1(columnita) and Regla_1(valor):
-        PosiblesHorizontales = RetornaPosiblesHorizontal(matriz,int(filita)-1,int(columnita)-1)#
-        PosiblesVerticales = RetornaPosiblesVertical(matriz,int(filita)-1,int(columnita)-1)#
-        PosibleCuadricula = RetornaPosiblesCuadrante(matriz,int(filita)-1,int(columnita)-1)#
+    filaB = EntreUnoNueve(fila)
+    columnaB = EntreUnoNueve(columna)
+    valorB = EntreUnoNueve(valor)
 
+    if filaB and columnaB and valorB:
+        if ValidaCifra(matriz, valor, int(fila), int(columna)):
+            matriz[int(fila) - 1][int(columna) - 1] = valor
+        else:
+            print("El valor de la cifra no debe repetirse en fila, columna o cuadrante :) \n")
     else:
-        print()
-        print("Ingresa un valor entre 1 y 9 :D")
-        print()
-        print()
-        return continuaTablero(matriz)
-
-    Cumplimiento =False
-
-    if (valor in PosiblesHorizontales) and (valor in PosiblesVerticales) and (valor in PosibleCuadricula):
-        PosiblesNetos.append(valor)
-        Cumplimiento = True
-
-    if Regla_1(valor) and (valor in PosiblesNetos) and Cumplimiento:
-        matriz[int(filita)-1][int(columnita)-1] = valor
-    else:
-        print()
-        print("Ingresa un valor que cumpla las reglas :D")
-        print("*Recuerda que los números no se deben repetir")
-        print("en las filas, columnas ni cuadrantes de 3x3*")
-        print()
-        print()
+        if filaB:
+            print("El valor de la fila debe estar entre 1 y 9 :) \n")
+        if columnaB:
+            print("El valor de la columna debe estar entre 1 y 9 :) \n")
+        if valorB:
+            print("El valor de la casilla debe estar entre 1 y 9 :) \n")
 
     return continuaTablero(matriz)
-
--- Reglas.py
+++ Reglas.py
@@ -1,113 +1,33 @@
-def Regla_1(valor):
+def VerificaExistencia(archivo):
+    try:
+        with open(archivo, 'r') as f:
+            return True
+    except FileNotFoundError as e:
+        return False
+    except IOError as e:
+        return False
+
+def EntreUnoNueve(valor):
+    listNumeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     Cumple = False
-    if valor=="1" or valor=="2" or valor=="3" or valor=="4" or valor=="5" or valor=="6" or valor=="7" or valor=="8" or valor=="9":
+    if valor in listNumeros:
         Cumple = True
     return Cumple
 
-def RetornaCuadrante(fila, columna):
-    if fila <= 2 and columna <= 2:
-        return 1
-    elif fila <= 5 and columna <= 2:
-        return 4
-    elif fila <= 8 and columna <= 2:
-        return 7
-    elif fila <= 2 and columna <= 5:
-        return 2
-    elif fila <= 5 and columna <= 5:
-        return 5
-    elif fila <= 8 and columna <= 5:
-        return 8
-    elif fila <= 2 and columna <= 8:
-        return 3
-    elif fila <= 5 and columna <= 8:
-        return 6
-    elif fila <= 8 and columna <= 8:
-        return 9
-
-def filaCuadrante(cuadrante):
-    filai = (cuadrante - 1) // 3
-    listaFila = []
-    for i in range(filai * 3, filai * 3 + 3):
-        listaFila.append(i)
-    return listaFila
-
-def columnaCuadrante(cuadrante):
-    columnai = (cuadrante - 1) // 3
-    columnaFila = []
-    for i in range(columnai * 3, columnai * 3 + 3):
-        columnaFila.append(i)
-    return columnaFila
-
-def RetornaPosiblesVertical(tabla, fila, columna):
-    disponibles = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    filas = len(tabla)
-    for i in range(filas):
-        if i != fila:
-            valor = tabla[i][columna]
-            if valor in disponibles:
-                disponibles.remove(valor)
-    return disponibles
-
-def RetornaPosiblesHorizontal(tabla, fila, columna):
-    disponibles = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    columnas = len(tabla[0])
-    for i in range(columnas):
-        if i != columna:
-            valor = tabla[fila][i]  # valor que hay asignado
-            if valor in disponibles:  # si el valor que hemos leido esta en la lista
-                disponibles.remove(valor)  # lo borramos de la lista ya que no disponible
-    return disponibles
-
-def RetornaPosiblesCuadrante(tabla, fila, columna):
-    disponibles = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    cuadrante = RetornaCuadrante(fila, columna)
-    minifilas = filaCuadrante(cuadrante)
-    minicolumnas = columnaCuadrante(cuadrante)
-    valorinicialenpuntoestudio = tabla[fila][columna]
-    tabla[fila][columna] = ''
-    for i in minifilas:
-        for j in minicolumnas:
-            if tabla[i][j] != '':
-                valor = tabla[i][j]
-                if valor in disponibles:
-                    disponibles.remove(valor)
-    tabla[fila][columna] = valorinicialenpuntoestudio  # volvemos a poner el valor inicial
-    return disponibles
-
-# Actualizacion_1.5
-
-
-def Resolver_cifra(sudoku):
-    encontrar = Encontrar_vacio(sudoku)
-    if not encontrar:
-        return True
-    else:
-        fila, col = encontrar
-    listNumeros=["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    for num in listNumeros:
-        if validar_cifra(sudoku, num, fila, col):
-            sudoku[fila][col] = num
-            if Resolver_cifra(sudoku):
-                return True
-            sudoku[fila][col] = "0"
-    return False
-
-def Encontrar_vacio(sudoku):
-    for i in range(len(sudoku)):
-        for j in range(len(sudoku[0])):
+def BuscaVacio(sudoku):
+    for i in range(9):
+        for j in range(9):
             if sudoku[i][j] == "0":
-                return i, j  # fila, columna
+                return i, j
     return False
 
-def validar_cifra(sudoku, num, fila, col):
-    #Comprobar fila y col
+def ValidaCifra(sudoku, num, fila, col):
     for i in range(9):
         if sudoku[fila][i] == num and col != i:
             return False
         if sudoku[i][col] == num and fila != i:
             return False
 
-    # Casilla de verificación
     casilla_x = col // 3
     casilla_y = fila // 3
     for i in range(casilla_y*3, casilla_y*3 + 3):
@@ -116,6 +36,23 @@ def validar_cifra(sudoku, num, fila, col):
                 return False
 
     return True
+
+
+#BackTracking
+def ResuelveCifra(sudoku):
+    encontrar = BuscaVacio(sudoku)
+    if not encontrar:
+        return True
+    else:
+        fila, col = encontrar
+    listNumeros=["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for num in listNumeros:
+        if ValidaCifra(sudoku, num, fila, col):
+            sudoku[fila][col] = num
+            if ResuelveCifra(sudoku):
+                return True
+            sudoku[fila][col] = "0"
+    return False
 
 
 
